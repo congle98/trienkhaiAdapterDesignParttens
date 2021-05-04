21e34ef1d63d4dcec0c639b3a802ce41c7df6aa0
congle98 <79907265+congle98@users.noreply.github.com>
2021-05-04 21:42:25 +0700

    addfile cách triển khai adapterDesignPartten

---
+++ src/views/Client.java
@@ -0,0 +1,29 @@
+package views;
+
+import model.FileCalculator;
+import model.FileCalculatorAdapter;
+
+import java.util.Scanner;
+
+public class Client {
+    private FileCalculator fileCalculator;
+
+    public Client(FileCalculator fileCalculator) {
+        this.fileCalculator = fileCalculator;
+    }
+
+    public void printFileSize(String path){
+        long size = fileCalculator.calculateSize(path);
+        System.out.println("Size: " + size);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter file or folder path: ");
+        String path = scanner.nextLine();
+        FileCalculator fileCalculator = new FileCalculatorAdapter();
+        Client client = new Client(fileCalculator);
+        client.printFileSize(path);
+
+    }
+}
